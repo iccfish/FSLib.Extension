b044a007c2643d42ff8b145c532fdbdfc1c225c0
iFish <iccfish@qq.com>
2014-07-16 21:48:54 +0800

    add multiple SynchorizonExtension

--- src/Threading/SynchorizonExtension.cs
+++ src/Threading/SynchorizonExtension.cs
@@ -11,6 +11,67 @@
 		{
 			context.Send(callback, null);
 		}
+		/// <summary>
+		/// 异步一个不需要参数的回调到目标线程
+		/// </summary>
+		/// <param name="context"></param>
+		/// <param name="callback"></param>
+		public static void Post(this SynchronizationContext context, SendOrPostCallback callback)
+		{
+			context.Post(callback, null);
+		}
+
+		/// <summary>
+		/// 同步一个不需要参数的回调到目标线程
+		/// </summary>
+		/// <param name="context"></param>
+		/// <param name="callback"></param>
+		public static void Send(this SynchronizationContext context, Action callback)
+		{
+			if (callback == null)
+				return;
+
+			context.Send(_ => callback(), null);
+		}
+
+		/// <summary>
+		/// 异步一个不需要参数的回调到目标线程
+		/// </summary>
+		/// <param name="context"></param>
+		/// <param name="callback"></param>
+		public static void Post(this SynchronizationContext context, Action callback)
+		{
+			if (callback == null)
+				return;
+
+			context.Post(_ => callback(), null);
+		}
+
+		/// <summary>
+		/// 同步一个不需要参数的回调到目标线程
+		/// </summary>
+		/// <param name="context"></param>
+		/// <param name="callback"></param>
+		public static void Send<T>(this SynchronizationContext context, Action<T> callback, T arg)
+		{
+			if (callback == null)
+				return;
+
+			context.Send(_ => callback(arg), null);
+		}
+
+		/// <summary>
+		/// 异步一个不需要参数的回调到目标线程
+		/// </summary>
+		/// <param name="context"></param>
+		/// <param name="callback"></param>
+		public static void Post<T>(this SynchronizationContext context, Action<T> callback, T arg)
+		{
+			if (callback == null)
+				return;
+
+			context.Post(_ => callback(arg), null);
+		}
 	}
 }
 
