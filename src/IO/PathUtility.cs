13615aeb62980b5562f365c9ff71ed69f9faa17f
iFish <iccfish@qq.com>
2014-11-18 23:27:54 +0800

    add PathUtility.cs

---
+++ src/IO/PathUtility.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System.IO
+{
+	public class PathUtility
+	{
+		/// <summary>
+		/// 合并路径
+		/// </summary>
+		/// <param name="paths"></param>
+		/// <returns></returns>
+		public static string Combine(params string[] paths)
+		{
+			return Path.GetFullPath(Path.Combine(paths));
+		}
+	}
+}
