87163af721f1c6383b9627ce69bbc02cb44b9e1d
iFish <iccfish@qq.com>
2015-04-16 21:05:40 +0800

    添加创建匿名字典类型辅助函数

--- src/FishLib/CollectionUtility.cs
+++ src/FishLib/CollectionUtility.cs
@@ -31,7 +31,31 @@ namespace System.FishLib
 		/// <returns></returns>
 		public static List<T> CreateAnymousTypeList<T>(T typeObj, int length = 10)
 		{
-			return new List<T>(10);
+			return new List<T>(length);
+		}
+
+		/// <summary>
+		/// 创建匿名类型的列表对象
+		/// </summary>
+		/// <typeparam name="T">匿名类型</typeparam>
+		/// <param name="typeObj">匿名对象实例</param>
+		/// <returns></returns>
+		public static HashSet<T> CreateAnymousTypeHashSet<T>(T typeObj)
+		{
+			return new HashSet<T>();
+		}
+
+		/// <summary>
+		/// 根据类型推断的结果创建一个泛型字典
+		/// </summary>
+		/// <typeparam name="TKey"></typeparam>
+		/// <typeparam name="TValue"></typeparam>
+		/// <param name="key"></param>
+		/// <param name="value"></param>
+		/// <returns></returns>
+		public static Dictionary<TKey, TValue> CreateAnymousDictionary<TKey, TValue>(TKey key, TValue value)
+		{
+			return new Dictionary<TKey, TValue>();
 		}
 	}
 }
