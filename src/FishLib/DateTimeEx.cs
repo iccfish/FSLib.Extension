b0fee61e6a5e76941890d2f08b769464033fe3d6
木魚(iFish) <iccfish@qq.com>
2018-12-18 15:15:46 +0800

    添加二进制序列化辅助类；增加字体加载辅助类；增加日期辅助函数；更新项目文件

---
+++ src/FishLib/DateTimeEx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FSLib.Extension.FishLib
+{
+	/// <summary>
+	/// 对时间的一些辅助工具方法
+	/// </summary>
+	public class DateTimeEx
+	{
+		/// <summary>
+		/// 从UNIX时间戳中获得市级的时间
+		/// </summary>
+		/// <param name="ticks"></param>
+		/// <returns></returns>
+		public static DateTime FromUnixTicks(long ticks) => FishDateTimeExtension.JsTicksStartBase.AddSeconds(ticks);
+
+		/// <summary>
+		/// 从JS的时间戳中获得时间
+		/// </summary>
+		/// <param name="ticks"></param>
+		/// <returns></returns>
+		public static DateTime FromJsTicks(long ticks) => FishDateTimeExtension.JsTicksStartBase.AddMilliseconds(ticks);
+	}
+}
---
+++ src/FishLib/Drawing/FontUtility.cs
@@ -0,0 +1,31 @@
+﻿#if NETCLR
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FSLib.Extension.FishLib.Drawing
+{
+	using System.Drawing;
+	using System.Drawing.Text;
+
+	class FontUtility
+	{
+		static FontFamily LoadRawFonts(byte[] buffer)
+		{
+			var f = new PrivateFontCollection();
+			unsafe
+			{
+				fixed (byte* pfont04b = buffer)
+				{
+					f.AddMemoryFont((IntPtr)pfont04b, buffer.Length);
+				}
+			}
+
+			return f.Families[0];
+		}
+
+	}
+}
+#endif
