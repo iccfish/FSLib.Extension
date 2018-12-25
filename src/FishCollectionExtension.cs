8138bdb0ec3c598d9b18b3171941e93216c9721b
木魚(iFish) <iccfish@qq.com>
2018-12-26 00:33:33 +0800

    增加针对KeyValuePair的解构方法

--- src/FishCollectionExtension.cs
+++ src/FishCollectionExtension.cs
@@ -757,6 +757,11 @@ namespace System
 			fifth = args[4];
 			sixth = args[5];
 		}
+		public static void Deconstruct<TKey, TValue>(this KeyValuePair<TKey, TValue> kvp, out TKey key, out TValue value)
+		{
+			key = kvp.Key;
+			value = kvp.Value;
+		}
 
 		#endregion
 	}
