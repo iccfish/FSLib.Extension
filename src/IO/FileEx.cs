a366f6595b02b0fdea4dd840f28f29957a76cb3e
木魚(iFish) <iccfish@qq.com>
2024-01-09 14:25:34 +0800

    新增文件、文件夹快捷操作

---
+++ src/IO/FileEx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FSLib.Extension.IO
+{
+    using System.IO;
+
+    /// <summary>
+    /// 针对文件处理的公共类
+    /// </summary>
+    public static class FileEx
+    {
+        /// <summary>
+        /// 将指定路径文件重命名为指定的名字
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="name"></param>
+        public static void RenameTo(string path, string name) => File.Move(path, Path.Combine(Path.GetDirectoryName(path), name));
+    }
+}
---
+++ src/IO/FolderEx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FSLib.Extension.IO
+{
+    using System.IO;
+
+    /// <summary>
+    /// 针对文件夹处理的公共类
+    /// </summary>
+    public static class FolderEx
+    {
+        /// <summary>
+        /// 将指定路径文件夹重命名为指定的名字
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="name"></param>
+        public static void RenameTo(string path, string name) => Directory.Move(path, Path.Combine(Path.GetDirectoryName(path), name));
+    }
+}
